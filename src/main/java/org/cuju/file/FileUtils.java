a8b94cf745cae1d64a796d7055937608e63073a7
Uoolo <wuchao@uoolo.com>
2020-04-27 10:34:08 +0800

    '下载图片并压缩'

--- src/main/java/org/cuju/file/FileUtils.java
+++ src/main/java/org/cuju/file/FileUtils.java
@@ -1,9 +1,121 @@
 package org.cuju.file;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
- * @Author: WuChao
- * @Description: TODO
+ * @Author: Charles
+ * @Description: 文件处理
  * @Date: Create in 17:27 2020/4/26
  */
 public class FileUtils {
+    private static Logger log = LoggerFactory.getLogger(FileUtils.class);
+
+    public static boolean packageZip(File zipFile, List<String> listKey){
+        //图片打包操作
+        ZipOutputStream zipStream = null;
+        FileInputStream zipSource = null;
+        BufferedInputStream bufferStream = null;
+        try {
+            zipStream = new ZipOutputStream(new FileOutputStream(zipFile));// 用这个构造最终压缩包的输出流
+//            zipSource = null;// 将源头文件格式化为输入流
+
+            for (String picKey : listKey) {
+
+                File file = new File(picKey);
+                //log.info("uppic zipFile: " + picKey );
+                zipSource = new FileInputStream(file);
+
+                byte[] bufferArea = new byte[1024 * 10];// 读写缓冲区
+
+                // 压缩条目不是具体独立的文件，而是压缩包文件列表中的列表项，称为条目，就像索引一样
+                ZipEntry zipEntry = new ZipEntry(file.getName());
+                zipStream.putNextEntry(zipEntry);// 定位到该压缩条目位置，开始写入文件到压缩包中
+
+                bufferStream = new BufferedInputStream(zipSource, 1024 * 10);// 输入缓冲流
+                int read = 0;
+
+                // 在任何情况下，b[0] 到 b[off] 的元素以及 b[off+len] 到 b[b.length-1]
+                // 的元素都不会受到影响。这个是官方API给出的read方法说明，经典！
+                while ((read = bufferStream.read(bufferArea, 0, 1024 * 10)) != -1) {
+                    zipStream.write(bufferArea, 0, read);
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            log.error("zipStream下载文件报错：", e);
+            return false;
+        } finally {
+            // 关闭流
+            try {
+                if (null != bufferStream)
+                    bufferStream.close();
+                if (null != zipStream)
+                    zipStream.close();
+                if (null != zipSource)
+                    zipSource.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                //e.printStackTrace();
+                log.error("close stream下载文件报错：", e);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @Description : 下载图片并打包
+     * FileUtils
+     * @Return :
+     * @Author : Charles
+     * @Date : 2020/4/27 10:15
+    */
+    public void downloadImgZip(String downloadFilename,String localUrl,String[] imgs){
+        try {
+            HttpServletResponse response = null;
+            downloadFilename = URLEncoder.encode(downloadFilename, "UTF-8");//转换中文否则可能会产生乱码
+            response.setContentType("application/octet-stream");// 指明response的返回对象是文件流
+            response.setHeader("Content-Disposition", "attachment;filename=" + downloadFilename);// 设置在下载框默认显示的文件名
+            ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
+
+            if (imgs != null && imgs.length > 0) {
+                try {
+                    String[] files = imgs;
+                    for (int i = 0; i < files.length; i++) {
+                        URL url = new URL(files[i]);
+                        zos.putNextEntry(new ZipEntry(localUrl + i + ".jpg"));
+                        //FileInputStream fis = new FileInputStream(new File(files[i]));
+                        InputStream fis = url.openConnection().getInputStream();
+                        byte[] buffer = new byte[1024];
+                        int r = 0;
+                        while ((r = fis.read(buffer)) != -1) {
+                            zos.write(buffer, 0, r);
+                        }
+                        fis.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            zos.flush();
+            zos.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
 }
