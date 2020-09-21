dcd5e3f3e1635980aa439af14b8eb259c985072f
Uoolo <wuchao@uoolo.com>
2020-09-21 09:43:36 +0800

    周岁计算

--- src/main/java/org/cuju/date/DateUtils.java
+++ src/main/java/org/cuju/date/DateUtils.java
@@ -135,5 +135,39 @@ public class DateUtils {
         return df.format(localDateTime);
     }
 
+   /**
+    * @Description : 根据生日计算当前周岁数
+    *
+    * @param birthday,targetDate
+    * @Return : 周岁
+    * @Author : Uoolo
+    * @Date : 2020/9/21 09:42
+   */
+    public int getCurrentAge(Date birthday,Date targetDate) {
+        // 当前时间
+        Calendar curr = Calendar.getInstance();
+        if(targetDate != null){
+            curr.setTime(targetDate);
+        }
+        // 生日
+        Calendar born = Calendar.getInstance();
+        born.setTime(birthday);
+        // 年龄 = 当前年 - 出生年
+        int age = curr.get(Calendar.YEAR) - born.get(Calendar.YEAR);
+        if (age <= 0) {
+            return 0;
+        }
+        // 如果当前月份小于出生月份: age-1
+        // 如果当前月份等于出生月份, 且当前日小于出生日: age-1
+        int currMonth = curr.get(Calendar.MONTH);
+        int currDay = curr.get(Calendar.DAY_OF_MONTH);
+        int bornMonth = born.get(Calendar.MONTH);
+        int bornDay = born.get(Calendar.DAY_OF_MONTH);
+        if ((currMonth < bornMonth) || (currMonth == bornMonth && currDay <= bornDay)) {
+            age--;
+        }
+        return age < 0 ? 0 : age;
+    }
+
 
 }
