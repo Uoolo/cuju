5171ee30814051c28910cdbb7048617511bf4369
Uoolo <wuchao@uoolo.com>
2021-03-15 14:07:06 +0800

    正则表达式

--- src/main/java/org/cuju/study/regular/MainTest.java
+++ src/main/java/org/cuju/study/regular/MainTest.java
@@ -6,4 +6,11 @@ package org.cuju.study.regular;
  * @Date: Create in 13:59 2021/3/15
  */
 public class MainTest {
+    public static void main(String[] args) {
+        String a = "StringA_StringAB_StringABA_StringABAB";
+        System.out.println(a.replaceAll("",""));//结果String_String_String  ([ab]+)  ""
+
+        String b = "StringA_StringAB_StringABA_StringABAB";
+        System.out.println(b.replaceAll("",""));//StringA1_StringAB1_StringABA1_StringABAB1  ([ab]+)  $11
+    }
 }
