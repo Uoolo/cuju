333cb00f5f60eae02da01320174e9eeed8fb4464
Uoolo <wuchao@uoolo.com>
2021-03-19 20:32:26 +0800

    代理模式 cglib动态代理

---
+++ src/main/java/org/cuju/study/base/throwable/MainTest.java
@@ -0,0 +1,22 @@
+package org.cuju.study.base.throwable;
+
+/**
+ * @Author: WuChao
+ * @Description: 异常处理测试
+ * @Date: Create in 14:28 2021/3/18
+ */
+public class MainTest {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public void printMessage(){
+        //Class s = Class.forName("org.cuju.study.base.throwable.Computer");
+
+
+    }
+}
